8348f3ba2b3587f94d01bb6efcb06d5c66f10d45
orucmu <mustafaoruc720@gmail.com>
2022-11-03 23:08:46 +0300

    Latest

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ export default async function getData(number) {
    const {data: user} = await axios("https://jsonplaceholder.typicode.com/users/" + number);
    const {data: post} = await axios("https://jsonplaceholder.typicode.com/posts?userId=" + number);
 
-    console.log("User",user);
-    console.log("Post", post)
+    console.log("User:",user);
+    console.log("Post:", post)
     
 }
 
